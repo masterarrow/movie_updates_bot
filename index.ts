0a4b023ea993afb10bbde6e755822cee92d0fb70
masterarrow <masterarrows@gmail.com>
2021-01-08 21:10:27 +0200

    Telegram bot on Node.js and Telegraf.js

--- index.ts
+++ index.ts
@@ -11,24 +11,32 @@ dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const api = new API(process.env.TMDB_KEY);
 
+/* Display response in markdown format */
 async function response(ctx: TelegrafContext, movie: MovieI | boolean): Promise<void> {
-  if (!movie) {
-    await ctx.reply(errorResponse);
-  }
-  await ctx.replyWithMarkdown(markdownResponse(movie as MovieI));
+  try {
+    if (!movie) {
+      await ctx.reply(errorResponse);
+    }
+    await ctx.replyWithMarkdown(markdownResponse(movie as MovieI));
+  } catch(_) {}
 }
 
+/* Help command */
 bot.help((ctx) => ctx.reply('Send /start command or / to get a menu or /quit to leave'));
+/* Response for a sticker */
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 
+/* Commands: '/start' and '/'  */
 bot.start(ctx => ctx.replyWithHTML(welcomeMsg, getMainMenu()));
 bot.hears('/',ctx => ctx.replyWithHTML(welcomeMsg, getMainMenu()));
 
+/* Main menu buttons */
 bot.hears(keys.UPCOMING, async ctx => response(ctx, await api.getUpcoming()));
 bot.hears(keys.TOP_RATED, async ctx => response(ctx, await api.getTopRated()));
 bot.hears(keys.POPULAR, async ctx => response(ctx, await api.getPopular()));
 
 bot.on('text', async ctx => {
+  /* Response on any input except registered */
   await ctx.replyWithHTML(
     `Unknown command please choose one of the next`+
     `<i>${ctx.message.text}</i>`,
